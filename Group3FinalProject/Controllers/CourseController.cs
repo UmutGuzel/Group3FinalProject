af2fab9f9da3e349c265ab259fc6b3dc8236b50f
UmutGuzel <umut_guzel@yahoo.com>
2023-10-11 22:49:15 +0300

    HTML pages are added

---
+++ Group3FinalProject/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group3FinalProject.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
