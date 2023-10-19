18908288eeb6d53d6b670d0f79fbf96f9c382f75
UmutGuzel <umut_guzel@yahoo.com>
2023-10-19 23:09:08 +0300

    -

--- Group3FinalProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Group3FinalProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -115,14 +115,19 @@ namespace Group3FinalProject.Areas.Identity.Pages.Account
                 var user = CreateUser();
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+				await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+				
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
-                if (result.Succeeded)
+				
+
+				if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var userId = await _userManager.GetUserIdAsync(user);
+					await _userManager.AddToRoleAsync(user, "User");
+
+					var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
---
+++ Group3FinalProject/Migrations/20231019194438_FinalMigration.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Group3FinalProject.Migrations
+{
+    public partial class FinalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Categories_CategoryId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Enrollments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseId",
+                table: "Enrollments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Courses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Courses",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_UserId",
+                table: "Courses",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_AspNetUsers_UserId",
+                table: "Courses",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Categories_CategoryId",
+                table: "Courses",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_AspNetUsers_UserId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Categories_CategoryId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_UserId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Enrollments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseId",
+                table: "Enrollments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Categories_CategoryId",
+                table: "Courses",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
